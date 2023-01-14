f34aeac694a5a124f38223cd73d02e866f50069c
Farhan Firmansyah <121080312+frhnfrman@users.noreply.github.com>
2023-01-14 21:19:45 +0700

    kurang sorting,search,deleting\read belum sempurna

--- #projek python toko furniture.py
+++ #projek python toko furniture.py
@@ -2,51 +2,57 @@
 while True:
     print("Apakah anda Pembeli atau Penjual ?")
     openp = str(input())
-    if openp in ["Pembeli","pembeli"]:
-        f = open("opening pembeli.txt","r")
-        bacabeliopen=f.read
-        print(bacabeliopen)
-        f.close()
-        print("tulis nomor dibawah ini untuk melakukan aksi")
-        print("1.Mencari dan melihat Barang\n2.Memesan barang\n3.Keluar Aplikasi")
+    if openp in ["Pembeli","pembeli"]:  
+        print("="*60)
+        print("\tSelamat datang di Toko furniture\n\t\tSelamat Berbelanja")
+        print("="*60)
+        print("Tulis nomor dibawah ini untuk melakukan aksi")
+        print("1.Melihat Tampilan Barang\n2.Mencari Barang\n3.Keluar Aplikasi")
         aksi_beli = int(input("=>"))
         if aksi_beli == 1:
-            f = open("deskripsi brng.txt","r") 
-            deskrip_brng= f.read()
-            print(deskrip_brng)
-            Keluar1 = str(input(("Apakah Anda Ingin Kembali Ke halaman Utama?(Ya/Tidak)  ")))
-            if Keluar1 in ["Ya","ya","y"]:
-                pass
-            else:
-                break
+            with open ("deskripsi brng.txt",'r') as f:
+                baca_brng = f.readlines()
+                list_brng = [baca_brng[0],baca_brng[1],baca_brng[2]]
+                for a in baca_brng:
+                    print(f"Nama Perabotan\t: {list_brng[0]}")
+                    print(f"Harga Perabotan\t: {list_brng[1]}")
+                    print(f"Material\t: {list_brng[2]}")
+                    print("-"* 60)
 
+        #elif aksi_beli == 2:
+            
 
-        elif aksi_beli == 2:
-            print('no')
         elif aksi_beli == 3:
-            pass
+            break
         else:
-            print('inputan salah,coba lagi')
+         print('inputan salah,coba lagi')
 
 
 
     elif openp in ["Penjual","penjual"]:
-        f = open("opening penjual.txt","r")
-        bacajualopen=f.read()
-        print(bacajualopen)
+        print("="*60)
+        print("\tSelamat datang Penjual terhormat")
+        print("="*60)
         print("tulis nomor dibawah ini untuk melakukan aksi")
-        print("1.Menambah/ menjual barang\n2.info pemesan barang\n3.Keluar Aplikasi")
+        print("1.Menambah/ menjual barang\n2.Keluar Aplikasi")
         aksi_jual=int(input('=>'))
         if aksi_jual == 1:
-            nama_brng=str(input('masukkan nama barang'))
-            harga_brng=int(input('masukkan harga yang ngotak'))
-            material_brng=str(input('masukkan material barang'))
+            nama_brng=str(input('masukkan nama barang = '))
+            harga_brng=int(input('masukkan harga yang ngotak = '))
+            material_brng=str(input('masukkan material barang = '))
+            deskrip_baru="{}${}${}".format(nama_brng,harga_brng,material_brng)
             f = open('deskripsi brng.txt','a')
-            deskrip_baru="name = {} , harga = {} , material = {}".format(nama_brng,harga_brng,material_brng)
-            f.write("\n") 
+            f.write("\n")
             f.write(deskrip_baru)
-            print("Data sudah dimasukkan")
+            print("Data tersimpan")
             f.close
+            keluar2 = str(input("Apakah Anda Ingin Kembali Ke halaman Utama?(Ya/Tidak)  "))
+            if keluar2 in ["Ya","ya","y"]:
+                print('data akan disimpan setelah keluar aplikasi')
+                continue
+            else:
+                print('data sudah dismpan')
+                break
         elif aksi_jual == 2:
             print('no')
         elif aksi_jual == 3:
